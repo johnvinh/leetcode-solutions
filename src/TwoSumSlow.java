b0249833e03812ee83b7b8a1c9e5cc1c26321ba2
John Vinh <61337643+johnvinh@users.noreply.github.com>
2021-11-25 11:17:43 -0500

    solved two sum with time complexity of o(n^2)

---
+++ src/TwoSumSlow.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class TwoSumSlow {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
