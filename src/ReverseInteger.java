7d1639d0f09dd3fe42f29b692c51ee562e200fac
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-26 14:30:41 -0500

    reversed numbers that go outside of the 32 bit range now return 0 instead per the problem specifications

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,14 +1,30 @@
 import java.util.Stack;
 
 public class ReverseInteger {
-    public int reverse(int x) {
+    public static int reverse(int x) {
         String xString = Integer.toString(x);
         Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < xString.length(); i++)
-            stack.push(xString.charAt(i));
         StringBuilder out = new StringBuilder();
+        if (xString.charAt(0) == '-') {
+            out.append('-');
+            for (int i = 1; i < xString.length(); i++)
+                stack.push(xString.charAt(i));
+        }
+        else {
+            for (int i = 0; i < xString.length(); i++)
+                stack.push(xString.charAt(i));
+        }
         while (!stack.empty())
             out.append(stack.pop());
-        return Integer.parseInt(out.toString());
+        try {
+            return Integer.parseInt(out.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int input = 1534236469;
+        System.out.println(reverse(input));
     }
 }
