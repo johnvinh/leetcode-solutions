0b265215e244565a98776a7f9018f77b0480ab32
John Vinh <john.michael.vinh2@gmail.com>
2022-12-15 21:33:40 -0500

    Solve Product of Array Except Self

--- src/ProductOfArrayExceptSelf.java
+++ src/ProductOfArrayExceptSelf.java
@@ -1,15 +1,15 @@
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int[] products = new int[nums.length];
+        int product = 1;
         for (int i = 0; i < nums.length; i++) {
-            int product = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (j == i) {
-                    continue;
-                }
-                product *= nums[j];
-            }
             products[i] = product;
+            product *= nums[i];
+        }
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            products[i] *= product;
+            product *= nums[i];
         }
         return products;
     }
