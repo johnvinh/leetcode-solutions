b713c9715d765e2211ee457e7abd43357f8e2951
John Vinh <john.michael.vinh2@gmail.com>
2022-12-15 21:14:51 -0500

    Solve in quadratic time

--- src/ProductOfArrayExceptSelf.java
+++ src/ProductOfArrayExceptSelf.java
@@ -1,2 +1,16 @@
 public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] products = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                product *= nums[j];
+            }
+            products[i] = product;
+        }
+        return products;
+    }
 }
