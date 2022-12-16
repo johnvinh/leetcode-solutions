80fc17b80158eabe76de46e2090237348dea3051
John Vinh <john.michael.vinh2@gmail.com>
2022-12-15 20:08:10 -0500

    Add test case for second example

--- src/BestTimeToBuySellStockTest.java
+++ src/BestTimeToBuySellStockTest.java
@@ -9,4 +9,9 @@ class BestTimeToBuySellStockTest {
     public void example1() {
         assertEquals(5, obj.maxProfit(new int[]{7,1,5,3,6,4}));
     }
+
+    @Test
+    public void example2() {
+        assertEquals(0, obj.maxProfit(new int[]{7,6,4,3,1}));
+    }
 }
