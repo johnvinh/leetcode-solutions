17f03bbfefb8e0769d7d5b831f07f0a443c6bc7a
John Vinh <john.michael.vinh2@gmail.com>
2022-12-15 21:14:00 -0500

    Create test for first example

--- src/ProductOfArrayExceptSelfTest.java
+++ src/ProductOfArrayExceptSelfTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductOfArrayExceptSelfTest {
-
+    ProductOfArrayExceptSelf obj = new ProductOfArrayExceptSelf();
+    @Test
+    public void example1() {
+        assertArrayEquals(new int[]{24,12,8,6}, obj.productExceptSelf(new int[]{1, 2, 3, 4}));
+    }
 }
