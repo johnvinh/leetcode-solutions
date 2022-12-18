f2184e858037345c03b1e473133d9679b2d76065
John Vinh <john.michael.vinh2@gmail.com>
2022-12-17 21:25:28 -0500

    Add example tests

--- src/ValidAnagramTest.java
+++ src/ValidAnagramTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidAnagramTest {
+    ValidAnagram validAnagram = new ValidAnagram();
+    @Test
+    public void example1() {
+        assertTrue(validAnagram.isAnagram("anagram", "nagaram"));
+    }
 
+    @Test
+    public void example2() {
+        assertFalse(validAnagram.isAnagram("rat", "car"));
+    }
 }
