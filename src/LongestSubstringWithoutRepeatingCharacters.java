dc0b2d967a2581ffd3e3cf16f3f33d2c29c69edf
John Vinh <john.michael.vinh2@gmail.com>
2022-12-18 23:04:15 -0500

    Get correct output for examples

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,20 @@
+import java.util.Collections;
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> seenCharacters = new HashSet<>();
+        HashSet<Integer> lengths = new HashSet<>();
+        int currentLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (seenCharacters.contains(s.charAt(i))) {
+                lengths.add(currentLength);
+                currentLength = 0;
+                seenCharacters.clear();
+            }
+            seenCharacters.add(s.charAt(i));
+            currentLength++;
+        }
+        return Collections.max(lengths);
+    }
 }
