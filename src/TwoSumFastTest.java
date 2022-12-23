9abb97d413b55ade2a85293b30c4ba37ee030857
John Vinh <john.michael.vinh2@gmail.com>
2022-12-23 09:24:40 -0500

    Add first example unit test

--- src/TwoSumFastTest.java
+++ src/TwoSumFastTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumFastTest {
+    TwoSumFast twoSum = new TwoSumFast();
 
+    @Test
+    public void example1() {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        assertArrayEquals(new int[]{0, 1}, twoSum.twoSum(nums, target));
+    }
 }
