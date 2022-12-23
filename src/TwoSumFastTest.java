c020e4ff095f0ec0c6e23b18e755fff53e988186
John Vinh <john.michael.vinh2@gmail.com>
2022-12-23 09:38:06 -0500

    Add test for second example

--- src/TwoSumFastTest.java
+++ src/TwoSumFastTest.java
@@ -11,4 +11,11 @@ class TwoSumFastTest {
         int target = 9;
         assertArrayEquals(new int[]{1, 0}, twoSum.twoSum(nums, target));
     }
+
+    @Test
+    public void example2() {
+        int[] nums = new int[]{3, 2, 4};
+        int target = 6;
+        assertArrayEquals(new int[]{2, 1}, twoSum.twoSum(nums, target));
+    }
 }
