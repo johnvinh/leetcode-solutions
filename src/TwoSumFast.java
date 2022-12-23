e57158d0f388f612003d0069c67fda2715e99f81
John Vinh <john.michael.vinh2@gmail.com>
2022-12-23 09:36:27 -0500

    Implement HashMap solution

--- src/TwoSumFast.java
+++ src/TwoSumFast.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class TwoSumFast {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            int complement = Math.abs(difference - target);
+            if (map.containsKey(complement)) {
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(difference, i);
+        }
+        return new int[]{};
+    }
 }
