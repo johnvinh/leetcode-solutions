cab53efa31ca580fa6f35c1c47d9a43d56170ee5
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 18:45:23 -0500

    Create getLetterCounts method

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -2,11 +2,20 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class RansomNote {
+    public HashMap<Character, Integer> getLetterCounts(String input) {
+        HashMap<Character, Integer> letterCounts = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            Integer current = letterCounts.getOrDefault(input.charAt(i), 0);
+            letterCounts.put(input.charAt(i), current + 1);
+        }
+        return letterCounts;
+    }
+
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> magazineLetterCounts = new HashMap<>();
-        for (int i = 0; i < magazine.length(); i++) {
-            Integer current = magazineLetterCounts.getOrDefault(magazine.charAt(i), 0);
-            magazineLetterCounts.put(magazine.charAt(i), current + 1);
+        HashMap<Character, Integer> ransomNoteLetterCounts = getLetterCounts(ransomNote);
+        HashMap<Character, Integer> magazineLetterCounts = getLetterCounts(magazine);
+        for (int i = 0; i < ransomNoteLetterCounts.keySet().size(); i++) {
+            
         }
         return false;
     }
