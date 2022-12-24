8a5f406373b4c1397d3615b74f63da445d101413
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 18:52:15 -0500

    Add example tests

--- src/RansomNoteTest.java
+++ src/RansomNoteTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RansomNoteTest {
+    RansomNote ransomNote = new RansomNote();
 
+    @Test
+    public void example1() {
+        assertFalse(ransomNote.canConstruct("a", "b"));
+    }
+
+    @Test
+    public void example2() {
+        assertFalse(ransomNote.canConstruct("aa", "ab"));
+    }
+
+    @Test
+    public void example3() {
+        assertTrue(ransomNote.canConstruct("aa", "aab"));
+    }
 }
