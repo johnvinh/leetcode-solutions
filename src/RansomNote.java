3320e8fc1821e225ffe97a059e437b85555f59ee
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 18:40:15 -0500

    Store character counts of magazine in hashmap

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineLetterCounts = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            Integer current = magazineLetterCounts.getOrDefault(magazine.charAt(i), 0);
+            magazineLetterCounts.put(magazine.charAt(i), current + 1);
+        }
+        return false;
+    }
 }
