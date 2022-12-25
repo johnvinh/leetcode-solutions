917064a5aa63ff4b8327cb0937cebb8c0d9f47bd
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 19:28:01 -0500

    Add unit test for first example

--- src/GroupAnagramsTest.java
+++ src/GroupAnagramsTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupAnagramsTest {
+    GroupAnagrams groupAnagrams = new GroupAnagrams();
 
+    @Test
+    public void example1() {
+        String[] input = new String[]{"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> expected = Arrays.asList(
+                List.of("bat"),
+                Arrays.asList("nat","tan"),
+                Arrays.asList("ate","eat","tea")
+        );
+        assertEquals(expected, groupAnagrams.groupAnagrams(input));
+    }
 }
