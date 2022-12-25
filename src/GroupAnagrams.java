6b51abf4d4850659e14aad38829fe0f71254bbe0
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 19:11:27 -0500

    Add getLetterCounts method

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class GroupAnagrams {
+    public HashMap<Character, Integer> getLetterCounts(String input) {
+        HashMap<Character, Integer> letterCounts = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            Integer current = letterCounts.getOrDefault(input.charAt(i), 0);
+            letterCounts.put(input.charAt(i), current + 1);
+        }
+        return letterCounts;
+    }
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> out = new ArrayList<>();
+        return out;
+    }
 }
