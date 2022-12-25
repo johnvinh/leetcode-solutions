69531c40302a0a69ed350245c9d7ef61effbf88f
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 19:30:56 -0500

    Remove commented out code

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -13,12 +13,10 @@ public class GroupAnagrams {
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        //HashMap<String, HashMap<Character, Integer>> letterCounts = new HashMap<>();
         // Anagrams will have the same number of characters, thus the same letterCount hashmap
         HashMap<HashMap<Character, Integer>, ArrayList<String>> anagramGroups = new HashMap<>();
 
         for (int i = 0; i < strs.length; i++) {
-            //letterCounts.put(strs[i], getLetterCounts(strs[i]));
             HashMap<Character, Integer> letterCounts = getLetterCounts(strs[i]);
             anagramGroups.computeIfAbsent(letterCounts, k -> new ArrayList<>());
             anagramGroups.get(letterCounts).add(strs[i]);
