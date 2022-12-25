d0497ac0154226a28e27badadc1a168fed5eddee
John Vinh <john.michael.vinh2@gmail.com>
2022-12-24 19:30:19 -0500

    Solve Group Anagrams

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -11,9 +11,23 @@ public class GroupAnagrams {
         }
         return letterCounts;
     }
-    
+
     public List<List<String>> groupAnagrams(String[] strs) {
+        //HashMap<String, HashMap<Character, Integer>> letterCounts = new HashMap<>();
+        // Anagrams will have the same number of characters, thus the same letterCount hashmap
+        HashMap<HashMap<Character, Integer>, ArrayList<String>> anagramGroups = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            //letterCounts.put(strs[i], getLetterCounts(strs[i]));
+            HashMap<Character, Integer> letterCounts = getLetterCounts(strs[i]);
+            anagramGroups.computeIfAbsent(letterCounts, k -> new ArrayList<>());
+            anagramGroups.get(letterCounts).add(strs[i]);
+        }
         List<List<String>> out = new ArrayList<>();
+        for (HashMap<Character, Integer> group : anagramGroups.keySet()) {
+            ArrayList<String> anagramGroup = anagramGroups.get(group);
+            out.add(anagramGroup);
+        }
         return out;
     }
 }
