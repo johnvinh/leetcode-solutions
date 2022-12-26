e573472cc95f227e63e2545da523b150dc395459
John Vinh <john.michael.vinh2@gmail.com>
2022-12-25 19:23:03 -0500

    Add test for first example

--- src/PalindromeNumberTest.java
+++ src/PalindromeNumberTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeNumberTest {
-
+    PalindromeNumber palindromeNumber = new PalindromeNumber();
+    @Test
+    public void example1() {
+        assertTrue(palindromeNumber.isPalindrome(121));
+    }
 }
