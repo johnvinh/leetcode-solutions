4cda36f71487bf59232103acad489ff3ff6b34a6
John Vinh <john.michael.vinh2@gmail.com>
2022-12-25 21:16:19 -0500

    Start generateParenthesis method

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
-
+        return new ArrayList<>();
     }
 }
