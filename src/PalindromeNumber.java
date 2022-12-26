55beb52d116a5af6d9cb86235e6dab9522e13d11
John Vinh <john.michael.vinh2@gmail.com>
2022-12-25 19:23:15 -0500

    Solve non-negative examples

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class PalindromeNumber {
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        StringBuilder sb = new StringBuilder(String.valueOf(x));
+        System.out.println(sb);
+        return Integer.parseInt(sb.reverse().toString()) == x;
+    }
 }
