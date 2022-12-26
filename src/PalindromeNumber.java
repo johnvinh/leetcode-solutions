b69806234ba8b25934dbec03c93549752ee63959
John Vinh <john.michael.vinh2@gmail.com>
2022-12-25 19:24:08 -0500

    Solve Palindrome Number

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -2,6 +2,6 @@ public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         StringBuilder sb = new StringBuilder(String.valueOf(x));
         System.out.println(sb);
-        return Integer.parseInt(sb.reverse().toString()) == x;
+        return sb.reverse().toString().equals(String.valueOf(x));
     }
 }
