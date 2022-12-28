b747cf1b80862335014aee9efad3f4ac014abc05
John Vinh <john.michael.vinh2@gmail.com>
2022-12-27 20:15:26 -0500

    Solve examples

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,2 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> brackets = new Stack<>();
+        Map<Character, Character> closing = Map.ofEntries(
+                Map.entry('(', ')'),
+                Map.entry('{', '}'),
+                Map.entry('[', ']')
+        );
+        for (int i = 0; i < s.length(); i++) {
+            if (closing.containsKey(s.charAt(i))) { // Opening bracket
+                brackets.add(s.charAt(i));
+            } else { // Closing bracket
+                Character openingBracket = brackets.pop();
+                if (s.charAt(i) != closing.get(openingBracket)) {
+                    return false;
+                }
+            }
+        }
+        return brackets.isEmpty();
+    }
 }
