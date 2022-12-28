f0d7ca0ce3fbe138ea230bfdbde112c71128fbf6
John Vinh <john.michael.vinh2@gmail.com>
2022-12-27 20:14:34 -0500

    Add unit test examples

--- src/ValidParenthesesTest.java
+++ src/ValidParenthesesTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidParenthesesTest {
+    ValidParentheses validParentheses = new ValidParentheses();
+    @Test
+    public void example1() {
+        assertTrue(validParentheses.isValid("()"));
+    }
 
+    @Test
+    public void example2() {
+        assertTrue(validParentheses.isValid("()[]{}"));
+    }
+
+    @Test
+    public void example3() {
+        assertFalse(validParentheses.isValid("(]"));
+    }
 }
