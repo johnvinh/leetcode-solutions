5ae22f995627ed3fb62253729a67402529c6e148
John Vinh <john.michael.vinh2@gmail.com>
2022-12-31 21:55:35 -0500

    Add remaining tests for examples

--- src/WordPatternTest.java
+++ src/WordPatternTest.java
@@ -9,4 +9,14 @@ class WordPatternTest {
     public void example1() {
         assertTrue(wordPattern.wordPattern("abba", "dog cat cat dog"));
     }
+
+    @Test
+    public void example2() {
+        assertFalse(wordPattern.wordPattern("abba", "dog cat cat fish"));
+    }
+
+    @Test
+    public void example3() {
+        assertFalse(wordPattern.wordPattern("aaaa", "dog cat cat dog"));
+    }
 }
