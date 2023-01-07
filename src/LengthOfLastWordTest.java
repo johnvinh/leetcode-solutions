fcdd7b0c123d820bc4c40b28bedca72d400fadd0
John Vinh <john.michael.vinh2@gmail.com>
2023-01-06 23:02:50 -0500

    Implement tests for examples

--- src/LengthOfLastWordTest.java
+++ src/LengthOfLastWordTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LengthOfLastWordTest {
+    LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
 
+    @Test
+    public void example1() {
+        assertEquals(lengthOfLastWord.lengthOfLastWord("Hello World"), 5);
+    }
+
+    @Test
+    public void example2() {
+        assertEquals(lengthOfLastWord.lengthOfLastWord("   fly me   to   the moon  "), 4);
+    }
+
+    @Test
+    public void example3() {
+        assertEquals(lengthOfLastWord.lengthOfLastWord("luffy is still joyboy"), 6);
+    }
 }
