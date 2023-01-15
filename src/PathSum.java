9da5ce71440239e08ea1e7012f63f4c4914eec69
John Vinh <john.michael.vinh2@gmail.com>
2023-01-14 22:51:13 -0500

    Add TreeNode class

--- src/PathSum.java
+++ src/PathSum.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class PathSum {
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        TreeNode current = root;
+        return false;
+    }
 }
